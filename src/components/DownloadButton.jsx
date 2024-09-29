e49c8a68215e12d5a96ef7e4beadc00d0d19a3fd
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2024-09-29 21:19:58 +0600

    update on cv

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -5,25 +5,21 @@ import DownloadingIcon from '@mui/icons-material/Downloading';
 
 const DownloadButton = () => {
   const handleDownload = () => {
-  // Create a link element
-  const link = document.createElement('a')
-  link.href = '/cv.pdf' // Assuming the CV is in the public folder
-  link.download = 'swadhinbiswasCV.pdf'
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
+    // Create a link element
+    const link = document.createElement('a');
+    link.href = '../assets/cv.pdf'; // Assuming the CV is in the public folder
+    link.download = 'swadhinbiswasCV.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
   return (
     <Button
-      component="label"
-      role={undefined}
       variant="contained"
       tabIndex={-1}
       onClick={handleDownload}
-      startIcon={<DownloadingIcon />
-
-      }
+      startIcon={<DownloadingIcon />}
     >
       <span style={{ fontWeight: 'bold' }}>Download CV</span>
     </Button>
