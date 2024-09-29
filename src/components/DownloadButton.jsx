c10e3013c29b23436d25f2c44af49e783f378927
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2024-09-29 21:29:02 +0600

    update

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -5,7 +5,7 @@ import DownloadingIcon from '@mui/icons-material/Downloading';
 const downloadLink = document.createElement('a'); // Create the link outside the component
 
 const DownloadButton = () => {
-  downloadLink.href = '../assets/cv.pdf'; // Assuming the CV is in the public folder
+  downloadLink.href = 'src/assets/cv.pdf'; // Assuming the CV is in the public folder
   downloadLink.download = 'swadhinbiswasCV.pdf';
 
   const handleClick = () => {
